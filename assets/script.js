f2a4537c73cb62e0b5b9b19f5cafe3c0c95a8542
Alexa Hines <99751726+a-hines@users.noreply.github.com>
2022-07-06 20:32:21 -0400

    Title and description added to HTML

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 var getPlaylists = function () {
 
-    var response = fetch("https://api.github.com/users/octocat/repos");
+    var response = fetch("https://api.spotify.com/v1/playlists/3cEYpjA9oz9GiPac4AsH4n");
 console.log(response);
 };
 
