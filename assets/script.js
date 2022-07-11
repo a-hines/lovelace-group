62078a16b4b03a4413f37b8d8f12d1e916368829
matthew dehart <mattidehart@hotmail.com>
2022-07-11 18:02:04 -0400

    spotify api authorization

--- assets/script.js
+++ assets/script.js
@@ -17,40 +17,100 @@
 //     'Thunderstorm': ['dark', 'heavy', 'metal', 'death%20metal', 'hard%techno']
 // }
 
-var redirect_uri = "C:/Users/matti/Documents/Bootcamp/Projects/lovelace-group/index.html"
-var spotifyApi = new SpotifyWebApi({
-    clientId: 'ee9b9fc735074ad49bfd4a2c10247b48',
-    clientSecret: '9ec3e0120640458da0567d009217b4d9'
-});
+var redirect_uri = "http://127.0.0.1:5500/index.html"
 
-spotifyApi.clientCredentialsGrant().
-    then(function(result) {
-        console.log('It worked! Your access token is: ' + result.body.access_token); 
-    }).catch(function(err) {
-        console.log('If this is printed, it probably means that you used invalid ' +
-        'clientId and clientSecret values. Please check!');
-        // console.log('Hint: ');
-        // console.log(err);
-});
-    
-spotifyApi.clientCredentialsGrant().then(
-    function(data) {
-      console.log('The access token expires in ' + data.body['expires_in']);
-      console.log('The access token is ' + data.body['access_token']);
-  
-      // Save the access token so that it's used in future calls
-      spotifyApi.setAccessToken(data.body['access_token']);
-    },
-    function(err) {
-      console.log(
-        'Something went wrong when retrieving an access token',
-        err.message
-      );
+var client_id = "ee9b9fc735074ad49bfd4a2c10247b48";
+var client_secret = "9ec3e0120640458da0567d009217b4d9";
+var AUTHORIZE = "https://accounts.spotify.com/authorize";
+var TOKEN = "https://accounts.spotify.com/api/token";
+
+function onPageLoad(){
+    localStorage.setItem("client_id", client_id);
+    localStorage.setItem("client_secret", client_secret);
+
+    if (window.location.search.length > 0) {
+        handleRedirect();
+      }
+}
+
+function handleRedirect(){
+    let code = getCode();
+    fetchAccessToken( code );
+    window.history.pushState("", "", redirect_uri); // remove param from url
+}
+
+function getCode(){
+    let code = null;
+    const queryString = window.location.search;
+    if ( queryString.length > 0 ){
+        const urlParams = new URLSearchParams(queryString);
+        code = urlParams.get('code')
     }
-);
-    
-var authorize = "https://accounts.spotify.com/authorize";
-var token = "https://accounts.spotify.com/api/token";
+    return code;
+}
+
+function requestAuthorization(){
+    client_id = document.getElementById("clientId").value;
+    client_secret = document.getElementById("clientSecret").value;
+    localStorage.setItem("client_id", client_id);
+    localStorage.setItem("client_secret", client_secret); // In a real app you should not expose your client_secret to the user
+
+    let url = AUTHORIZE;
+    url += "?client_id=" + client_id;
+    url += "&response_type=code";
+    url += "&redirect_uri=" + encodeURI(redirect_uri);
+    url += "&show_dialog=true";
+    url += "&scope=user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private";
+    window.location.href = url; // Show Spotify's authorization screen
+}
+
+function fetchAccessToken( code ){
+    let body = "grant_type=authorization_code";
+    body += "&code=" + code; 
+    body += "&redirect_uri=" + encodeURI(redirect_uri);
+    body += "&client_id=" + client_id;
+    body += "&client_secret=" + client_secret;
+    callAuthorizationApi(body);
+}
+
+function refreshAccessToken(){
+    refresh_token = localStorage.getItem("refresh_token");
+    let body = "grant_type=refresh_token";
+    body += "&refresh_token=" + refresh_token;
+    body += "&client_id=" + client_id;
+    callAuthorizationApi(body);
+}
+
+function callAuthorizationApi(body){
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", TOKEN, true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.setRequestHeader('Authorization', 'Basic ' + btoa(client_id + ":" + client_secret));
+    xhr.send(body);
+    xhr.onload = handleAuthorizationResponse;
+}
+
+function handleAuthorizationResponse(){
+    if ( this.status == 200 ){
+        var data = JSON.parse(this.responseText);
+        console.log(data);
+        var data = JSON.parse(this.responseText);
+        if ( data.access_token != undefined ){
+            access_token = data.access_token;
+            localStorage.setItem("access_token", access_token);
+        }
+        if ( data.refresh_token  != undefined ){
+            refresh_token = data.refresh_token;
+            localStorage.setItem("refresh_token", refresh_token);
+        }
+        onPageLoad();
+    }
+    else {
+        console.log(this.responseText);
+        alert(this.responseText);
+    }
+}
+
 
 const clouds = ['techno', 'moody', 'house', 'rock'];
 const additional = ['folk', 'pop', 'hip%20hop', 'shoegaze', 'house', 'metal', 'classic%20rock'];
